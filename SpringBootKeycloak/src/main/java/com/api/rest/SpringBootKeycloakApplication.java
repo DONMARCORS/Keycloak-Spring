deb6fe6bc90f01a1eb2b542901869b53ed4bb1f2
Marco Rivera Silva <marcoantonioriverasilva@ciencias.unam.mx>
2024-07-05 22:03:14 -0600

    Add files via upload

---
+++ SpringBootKeycloak/src/main/java/com/api/rest/SpringBootKeycloakApplication.java
@@ -0,0 +1,15 @@
+package com.api.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@EnableCaching
+@SpringBootApplication
+public class SpringBootKeycloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKeycloakApplication.class, args);
+	}
+
+}
---
+++ SpringBootKeycloak/src/main/java/com/api/rest/controller/ProductController.java
@@ -0,0 +1,49 @@
+package com.api.rest.controller;
+
+//import org.apache.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.api.rest.model.Product;
+import com.api.rest.service.ProductService;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+
+@RestController
+@RequestMapping(name = "/products")
+public class ProductController {
+
+    @Autowired
+    ProductService productService;
+
+    @PostMapping("/create")
+    public ResponseEntity<?> createStudent(@RequestBody Product product) {
+        return new ResponseEntity<>(productService.createProduct(product), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<?> getProduct(@PathVariable Long id) {
+        return new ResponseEntity<>(productService.getProduct(id), HttpStatus.OK);
+    }
+
+    @PutMapping("product")
+    public ResponseEntity<?> updateProduct(@RequestBody Product entity) {
+        return new ResponseEntity<>(productService.updateProduct(entity), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/product/{id}")
+    public ResponseEntity<?> deleteProduct(@PathVariable Long id){
+        return new ResponseEntity<>(productService.deleteProduct(id), HttpStatus.OK);
+    }
+    
+}
